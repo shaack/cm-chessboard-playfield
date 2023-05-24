1ec55bef4405243debc45a1c9f45b096b21e6c80
shaack <shaack@gmail.com>
2023-05-24 15:48:17 +0200

    Premoves are working

--- src/players/LocalPlayerWithPremoves.js
+++ src/players/LocalPlayerWithPremoves.js
@@ -5,7 +5,7 @@
  */
 import {LocalPlayer} from "./LocalPlayer.js"
 import {Observed} from "cm-web-modules/src/observed/Observed.js"
-import {Chess} from "cm-chess/src/Chess.js"
+import {PlayfieldMarkers} from "../dependent-extensions/PlayfieldMarkers.js"
 
 export class LocalPlayerWithPremoves extends LocalPlayer {
     constructor(playfield, name) {
@@ -15,34 +15,48 @@ export class LocalPlayerWithPremoves extends LocalPlayer {
             premoveValidateEvent: null
         })
         this.state.addObserver((event) => {
-            console.log("state", event)
-        })
-        this.state.addObserver((event) => {
             this.playfield.chessboard.removePremoveMarkers()
-            if(event.value) {
+            if (event.value) {
                 this.playfield.chessboard.addPremoveMarker(event.value.squareFrom)
                 this.playfield.chessboard.addPremoveMarker(event.value.squareTo)
+                // allow right click to cancel premove
+                const handleContextMenu = () => {
+                    this.state.premoveValidateEvent = null
+                }
+                this.playfield.chessboard.context.addEventListener("contextmenu", (event) => {
+                    event.preventDefault()
+                    handleContextMenu()
+                    this.playfield.chessboard.context.removeEventListener("contextmenu", handleContextMenu)
+                })
             }
         }, ["premoveValidateEvent"])
+        setTimeout(() => {
+            this.playfieldMarkers = this.playfield.chessboard.getExtension(PlayfieldMarkers)
+        })
     }
 
     moveRequest(moveResponse) {
         this.state.premoving = false
-        if(!this.playfield.chessboard.view.moveInputCallback) { // enable it only once on first request
+        if (!this.playfield.chessboard.view.moveInputCallback) { // enable it only once on first request
             this.playfield.chessboard.enableMoveInput((event) => {
                 return this.chessboardMoveInputCallback(event, moveResponse)
             }, this.playfield.props.playerColor)
         }
-        if(this.state.premoveValidateEvent) {
-            const tmpChess = new Chess(this.playfield.state.chess.fen())
-            const move = {from: this.state.premoveValidateEvent.squareFrom, to: this.state.premoveValidateEvent.squareTo}
-            const validMove = !!tmpChess.move(move)
-            if(validMove) {
+        if (this.state.premoveValidateEvent) {
+            const isMoveValid = super.onValidateMoveInput(this.state.premoveValidateEvent, (move) => {
+                moveResponse(move)
+                this.state.premoving = true
+                this.state.premoveValidateEvent = null
+            })
+            if(isMoveValid && !this.playfield.chessboard.isPromotionDialogShown()) {
                 moveResponse({
                     from: this.state.premoveValidateEvent.squareFrom,
                     to: this.state.premoveValidateEvent.squareTo
                 })
                 this.state.premoving = true
+            }
+            if(!isMoveValid && !this.playfield.chessboard.isPromotionDialogShown()) {
+                this.playfieldMarkers.markIllegalMove(this.state.premoveValidateEvent.squareFrom, this.state.premoveValidateEvent.squareTo)
             }
             this.state.premoveValidateEvent = null
         }
@@ -57,9 +71,9 @@ export class LocalPlayerWithPremoves extends LocalPlayer {
     }
 
     onValidateMoveInput(chessboardEvent, moveResponse) {
-        if(this.state.premoving) {
+        if (this.state.premoving) {
             this.state.premoveValidateEvent = chessboardEvent
-            return false // no validation when premoving, todo show promotion dialog, if needed
+            return false // no validation when premoving
         } else {
             return super.onValidateMoveInput(chessboardEvent, moveResponse)
         }
@@ -68,16 +82,16 @@ export class LocalPlayerWithPremoves extends LocalPlayer {
     onMoveInputFinished(chessboardEvent, moveResponse) {
         if (chessboardEvent.legalMove && !this.state.premoving) {
             if (!this.playfield.chessboard.isPromotionDialogShown()) {
-                this.handleMoveResponse(moveResponse,{
+                this.handleMoveResponse(moveResponse, {
                     from: chessboardEvent.squareFrom,
                     to: chessboardEvent.squareTo
                 })
             }
-            if(!this.state.premoving) {
+            if (!this.state.premoving) {
                 this.state.premoving = true
             }
         } else {
-            if(this.state.premoveValidateEvent) {
+            if (this.state.premoveValidateEvent) {
                 this.playfield.chessboard.setPosition(this.playfield.state.chess.fen())
             }
         }
